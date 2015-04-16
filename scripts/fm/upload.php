b844783ab237477aa5695a02c67c01dc779a4d34
Falcon <gdv_pnz@mail.ru>
2015-04-17 01:28:33 +0300

    Fix file format upload

--- scripts/fm/upload.php
+++ scripts/fm/upload.php
@@ -1,14 +1,16 @@
 ﻿<?php
    session_start();
    $uploaddir = "W:\\falcon_host\\downloads\\";
+   $types = array("php", "html");
    if(is_uploaded_file($_FILES['user_file']['tmp_name']))
    {
       $uploadfile = $uploaddir . basename($_FILES['user_file']['name']);
 	  $uploadfile = iconv("utf-8", "windows-1251", $uploadfile);
       $file_type = substr($_FILES['user_file']['name'], 1 + strrpos($_FILES['user_file']['name'], "."));
-	  
+	  if(!in_array($file_type, $types))
+	  {
          move_uploaded_file($_FILES['user_file']['tmp_name'], $uploadfile);
-
+	  }
    }
    unset($_SESSION['upload_key_value']);
 ?>
